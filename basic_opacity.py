ff6ef988278491913822afc1eae5c0d0b30a5e78
Najeem Muhammed <najeem@gmail.com>
2020-07-29 15:49:03 +0200

    basic opacity

---
+++ basic_opacity.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageDraw
+
+scale_factor = 4 # Resize scaling for antialiasing
+img_size = (500, 500) # Final image size desired
+
+img_size_scaled = tuple(x*scale_factor for x in img_size)
+
+img = Image.new("RGB", img_size_scaled)
+ic = ImageDraw.Draw(img, "RGBA") # Specified RGBA
+
+def draw_circle(x, y, r, color):
+    """Function to draw a circle at given center with given
+    radius and colour
+    """
+    x, y, r = int(x), int(y), int(r) # Converting possible floats to int
+    ic.ellipse([x-r, y-r, x+r, y+r], fill=color)
+
+for i in range(10):
+    draw_circle(
+        img_size_scaled[0]/2, img_size_scaled[1]/2,
+        img_size_scaled[0]/2*(10-i)/10,
+        (255, 0, 0, int(255*i/10))
+    )
+# You can pass in list of tuples too!
+
+img = img.resize(img_size, resample=Image.ANTIALIAS)
+img.save("basic_opacity.png")
